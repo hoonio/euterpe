8aa09b309ec9a2a28413e57d16af399838a2c1ca
Hoonio <hoonio@hoonio.com>
2015-04-22 23:21:25 +0100

    build interface functionality

---
+++ www/js/services.js
@@ -0,0 +1,25 @@
+angular.module('songhop.services', [])
+  .factory('User', function(){
+
+    var o = {
+      favorites: []
+    };
+
+    o.addSongToFavorites = function(song){
+      // make sure there's a song to add
+      if (!song) return false;
+
+      // add to favorites array
+      o.favorites.unshift(song);
+    }
+
+    o.removeSongFromFavorites = function(song, index){
+      //make sure there's a song to remove
+      if (!song) return false;
+
+      // remove from favorites array
+      o.favorites.splice(index, 1);
+    }
+
+    return o;
+  });
