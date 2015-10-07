decdeac39f0cb4f5d4e81659d6bd31066526128a
Hoonio <hoonio@hoonio.com>
2015-10-07 23:25:36 +0100

    open spotify link from favorites page

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -63,12 +63,16 @@ Controller for the discover page
 /*
 Controller for the favorites page
 */
-.controller('FavoritesCtrl', function($scope, User) {
+.controller('FavoritesCtrl', function($scope, $window, User) {
   // get the list of our favorites from the user service
   $scope.favorites = User.favorites;
 
   $scope.removeSong = function(song, index) {
     User.removeSongFromFavorites(song, index);
+  };
+
+  $scope.openSong = function(song) {
+    $window.open(song.open_url, "_system");
   };
 })
 
